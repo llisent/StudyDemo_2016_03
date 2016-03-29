7796928ebacaffcfefcb3a83ffd21f196cb561db
赵新 <zhaoxin@yonglibao.com>
2016-03-29 17:32:37 +0800

    no message

---
+++ StudyDemo_0301/Macros.h
@@ -0,0 +1,23 @@
+//
+//  Macros.h
+//  StudyDemo_0301
+//
+//  Created by Mrr on 16/3/29.
+//  Copyright © 2016年 lnzsbks. All rights reserved.
+//
+
+#ifndef Macros_h
+#define Macros_h
+
+/** UIColor*/
+#define RGBA(r,g,b,a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
+
+
+/** UIScreen*/
+#define ScreenBonds [UIScreen mainScreen].bounds
+#define ScreenWidth [UIScreen mainScreen].bounds.size.width
+#define ScreenHeight [UIScreen mainScreen].bounds.size.height
+
+
+
+#endif /* Macros_h */
